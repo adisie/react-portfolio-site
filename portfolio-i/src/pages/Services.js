7c39912b2eaadcd0aff062f6964134fa9d202bfa
Adisie Fenta <adisiefenta23@gmail.com>
2023-12-11 22:22:48 +0300

    some changes

--- portfolio-i/src/pages/Services.js
+++ portfolio-i/src/pages/Services.js
@@ -1,5 +1,6 @@
 
 import { FaRegHandshake } from "react-icons/fa"
+import { FaDesktop } from "react-icons/fa6";
 
 const Services = () => {
   return (
@@ -7,16 +8,18 @@ const Services = () => {
         <h3><FaRegHandshake className="service-icon"/>Services</h3>
         <div className="service-list">
             <div className="service">
-                <h4>UI/UI DESIGN</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolor veniam rem quo non officia laborum, aspernatur veritatis alias odit. Voluptates rerum optio veritatis itaque quam, unde ut atque illum cupiditate quidem sunt sint nostrum repellendus amet voluptatibus, temporibus beatae. Natus pariatur fugiat ab suscipit sint cum placeat facere doloribus.</p>
+                <FaDesktop className="service-i" />
+                <div className="service-description">
+                    <h4>Frontend Designer</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolor veniam rem quo non officia laborum, aspernatur veritatis alias odit. Voluptates rerum optio veritatis itaque quam, unde ut atque illum cupiditate quidem sunt sint nostrum repellendus amet voluptatibus, temporibus beatae. Natus pariatur fugiat ab suscipit sint cum placeat facere doloribus.</p>
+                </div>
             </div>
             <div className="service">
-                <h4>BACKEND DEVELOPMENT</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolor veniam rem quo non officia laborum, aspernatur veritatis alias odit. Voluptates rerum optio veritatis itaque quam, unde ut atque illum cupiditate quidem sunt sint nostrum repellendus amet voluptatibus, temporibus beatae. Natus pariatur fugiat ab suscipit sint cum placeat facere doloribus.</p>
-            </div>
-            <div className="service">
-                <h4>TUTORING</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolor veniam rem quo non officia laborum, aspernatur veritatis alias odit. Voluptates rerum optio veritatis itaque quam, unde ut atque illum cupiditate quidem sunt sint nostrum repellendus amet voluptatibus, temporibus beatae. Natus pariatur fugiat ab suscipit sint cum placeat facere doloribus.</p>
+                <FaDesktop className="service-i" />
+                <div className="service-description">
+                    <h4>Frontend Designer</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolor veniam rem quo non officia laborum, aspernatur veritatis alias odit. Voluptates rerum optio veritatis itaque quam, unde ut atque illum cupiditate quidem sunt sint nostrum repellendus amet voluptatibus, temporibus beatae. Natus pariatur fugiat ab suscipit sint cum placeat facere doloribus.</p>
+                </div>
             </div>
         </div>
     </div>
